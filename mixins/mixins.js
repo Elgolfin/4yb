6142bfc6e1e54348bc40d0527b8c30d99d40a3b3
Elgolfin <elgo@nicomputer.net>
2016-02-04 11:54:18 -0500

    Empty the form when creating a new account (by resetting the viewData shared property)

--- mixins/mixins.js
+++ mixins/mixins.js
@@ -1,8 +1,11 @@
 // Mixins
 exports.mixin = {
   methods: {
-    changeView: function (view) {
+    changeView: function (view, resetViewData) {
         console.log("Change view from " + this.$root.currentView + " to " + view);
+        if (resetViewData) {
+            this.$root.viewData = {};
+        }
         this.$root.currentView = view;
     }
   }
