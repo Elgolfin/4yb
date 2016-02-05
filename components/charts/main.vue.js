3d044a650d374d83c1234e16fed0326689949db2
Elgolfin <elgo@nicomputer.net>
2016-02-05 17:23:02 -0500

    Integrate chartjs

--- components/charts/main.vue.js
+++ components/charts/main.vue.js
@@ -1,10 +1,38 @@
 const app = require('remote').require('app');
 const jetpack = require('fs-jetpack').cwd(app.getAppPath());
 var Vue = require('vue');
+var Chart = require('../../js/Chart.min.js');
 
 exports.main = Vue.extend({
     data: function () {
-        return { title: "Charts" }
+        return { 
+            title: "Charts",
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [
+                    {
+                        label: "My First dataset",
+                        fillColor: "rgba(220,220,220,0.5)",
+                        strokeColor: "rgba(220,220,220,0.8)",
+                        highlightFill: "rgba(220,220,220,0.75)",
+                        highlightStroke: "rgba(220,220,220,1)",
+                        data: [65, 59, 80, 81, 56, 55, 40]
+                    },
+                    {
+                        label: "My Second dataset",
+                        fillColor: "rgba(151,187,205,0.5)",
+                        strokeColor: "rgba(151,187,205,0.8)",
+                        highlightFill: "rgba(151,187,205,0.75)",
+                        highlightStroke: "rgba(151,187,205,1)",
+                        data: [28, 48, 40, 19, 86, 27, 90]
+                    }
+                ]
+            }
+        }
     },
-    template: jetpack.read('./components/charts/main.vue.html')
+    template: jetpack.read('./components/charts/main.vue.html'),
+    ready: function(){
+        var ctx = document.getElementById("myChart").getContext("2d");
+        var myBarChart = new Chart(ctx).Bar(this.data);
+    }
 });
