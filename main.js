e5f3052e14a2a26b32b4814d65c63bd0c8832941
Elgolfin <elgo@nicomputer.net>
2016-02-09 21:03:28 -0500

    Add some dev dependencies packages for future needs

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* global process */
 const electron = require('electron');
 const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
@@ -16,17 +17,15 @@ var mainWindow = null;
 app.on('window-all-closed', function() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  console.log("Closing All");
-  
   if (process.platform != 'darwin') {
     app.quit();
   }
-  
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
+  
   // Create the browser window.
   mainWindow = new BrowserWindow({});
   mainWindow.maximize();
