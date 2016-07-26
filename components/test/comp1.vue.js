1715f74a9dbed31b39b4101eb6be3e6356afdd8c
Elgolfin <elgo@nicomputer.net>
2016-07-26 10:53:12 -0400

    Remove old files

--- components/test/comp1.vue.js
+++
@@ -1,9 +0,0 @@
-const app = require('electron').remote.app;
-const jetpack = require('fs-jetpack').cwd(app.getAppPath());
-var Vue = require('vue');
-
-exports.comp1 = Vue.extend({
-    created: function(){
-        this.$root.currentView = "comp4ybTransactions";
-    }
-});
