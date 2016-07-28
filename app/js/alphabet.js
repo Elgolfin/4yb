2b6054931210987d07410715d8c67fa47fe09357
Elgolfin <elgo@nicomputer.net>
2016-07-28 13:57:18 -0400

    Remove duplicated methods

--- app/js/alphabet.js
+++ app/js/alphabet.js
@@ -1,53 +1,20 @@
-exports.alphabet = {
+exports.Alphabet = {
   getNextEntry: function (input) {
     var lastIndexOf = input.lastIndexOf('.')
     if (lastIndexOf > -1) {
-      return input.substr(0, lastIndexOf + 1) + exports.alphabet.increments(input.substr(lastIndexOf + 1))
+      return input.substr(0, lastIndexOf + 1) + exports.Alphabet.increments(input.substr(lastIndexOf + 1))
     } else {
-      return exports.alphabet.increments(input)
+      return exports.Alphabet.increments(input)
     }
   },
+
   getParentPath: function (input) {
     if (input === undefined || input === null) {
       return ''
     }
     return input.substring(0, input.lastIndexOf('.'))
   },
-  /*
-  path = the path of the account (i.e. : a, a.a, a.b.c, etc.)
-  data = the list of the accounts (each account has a property called path which refers to its path in the tree)
-  */
-  buildFlatList: function (path, data, indent) {
-    var ret = []
-    indent = indent || 0
-    data.forEach(function (entry, index) {
-      if (entry.parent === path) {
-        let txtIndent = ''
-        for (let i = 0; i < indent; i++) {
-          txtIndent += '&dash;&dash;'
-        }
-        ret.push({account: entry, indent: txtIndent})
-        let children = exports.alphabet.buildFlatList(entry.path, data, indent + 1)
-        children.forEach(function (entry, index) {
-          ret.push({account: entry.account, indent: entry.indent})
-        })
-      }
-    })
-    return ret
-  },
-  /*
-  path = the path of the account (i.e. : a, a.a, a.b.c, etc.)
-  data = the list of the accounts (each account has a property called path which refers to its path in the tree)
-  */
-  buildTreeList: function (path, data) {
-    var ret = []
-    data.forEach(function (entry, index) {
-      if (entry.parent === path) {
-        ret.push({account: entry, children: exports.alphabet.buildTreeList(entry.path, data)})
-      }
-    })
-    return ret
-  },
+
   increments: function (str) {
     if (str === null || str === '') {
       return ''
@@ -61,7 +28,7 @@ exports.alphabet = {
       if (str.length === 1) {
         newString = 'aa'
       } else {
-        newString = exports.alphabet.increments(str.substr(0, str.length - 1)) + 'a'
+        newString = exports.Alphabet.increments(str.substr(0, str.length - 1)) + 'a'
       }
     } else {
       newString = str.substr(0, str.length - 1) + String.fromCharCode(lastChar)
--- test/unit/specs/alphabet.spec.js
+++ test/unit/specs/alphabet.spec.js
@@ -1,79 +1,55 @@
-import { alphabet } from 'app/js/alphabet'
+import { Alphabet } from 'app/js/Alphabet'
 
 describe('Alphabet', function () {
   describe('Method getNextEntry:', function () {
     it(' Returns b when input is a', function () {
-      alphabet.getNextEntry('a').should.equal('b')
+      Alphabet.getNextEntry('a').should.equal('b')
     })
     it(' Returns a.b when input is a.a', function () {
-      alphabet.getNextEntry('a.a').should.equal('a.b')
+      Alphabet.getNextEntry('a.a').should.equal('a.b')
     })
     it(' Returns a.aa when input is a.z', function () {
-      alphabet.getNextEntry('a.z').should.equal('a.aa')
+      Alphabet.getNextEntry('a.z').should.equal('a.aa')
     })
     it(' Returns ab when input is aa', function () {
-      alphabet.getNextEntry('aa').should.equal('ab')
+      Alphabet.getNextEntry('aa').should.equal('ab')
     })
   })
   describe('Method getParentPath:', function () {
     it(' Returns empty when input is undefined', function () {
-      alphabet.getParentPath().should.equal('')
+      Alphabet.getParentPath().should.equal('')
     })
     it(' Returns a when input is a.b', function () {
-      alphabet.getParentPath('a.b').should.equal('a')
+      Alphabet.getParentPath('a.b').should.equal('a')
     })
     it(' Returns empty when input is a', function () {
-      alphabet.getParentPath('a').should.equal('')
+      Alphabet.getParentPath('a').should.equal('')
     })
   })
   describe('Method increments:', function () {
     it(' Returns b when incrementing a', function () {
-      alphabet.increments('a').should.equal('b')
+      Alphabet.increments('a').should.equal('b')
     })
     it(' Returns aa when incrementing z', function () {
-      alphabet.increments('z').should.equal('aa')
+      Alphabet.increments('z').should.equal('aa')
     })
     it(' Returns when incrementing aa', function () {
-      alphabet.increments('aa').should.equal('ab')
+      Alphabet.increments('aa').should.equal('ab')
     })
     it(' Returns az when incrementing ay', function () {
-      alphabet.increments('ay').should.equal('az')
+      Alphabet.increments('ay').should.equal('az')
     })
     it(' Returns ba when incrementing az', function () {
-      alphabet.increments('az').should.equal('ba')
+      Alphabet.increments('az').should.equal('ba')
     })
     it(' Returns aaaaa when incrementing zzzz', function () {
-      alphabet.increments('zzzz').should.equal('aaaaa')
+      Alphabet.increments('zzzz').should.equal('aaaaa')
     })
     it(' Returns empty when incrementing null', function () {
-      alphabet.increments(null).should.equal('')
+      Alphabet.increments(null).should.equal('')
     })
     it(' Returns empty when incrementing empty', function () {
-      alphabet.increments('').should.equal('')
-    })
-  })
-  describe('Method buildFlatList:', function () {
-    it(' Returns empty when input is bad', function () {
-      var ret = [{path: 'a'}, {path: 'a.a'}]
-      var assert = []
-      alphabet.buildFlatList('a', ret).should.eql(assert)
-    })
-    it(' Returns expected flat list when input is good', function () {
-      var ret = [{path: 'a.a', parent: 'a'}, {path: 'a.a.a', parent: 'a.a'}]
-      var assert = [{account: {parent: 'a', path: 'a.a'}, indent: ''}, {account: {parent: 'a.a', path: 'a.a.a'}, indent: '&dash;&dash;'}]
-      alphabet.buildFlatList('a', ret).should.eql(assert)
-    })
-  })
-  describe('Method buildTreeList:', function () {
-    it(' Returns empty when input is bad', function () {
-      var ret = [{path: 'a'}, {path: 'a.a'}]
-      var assert = []
-      alphabet.buildTreeList('a', ret).should.eql(assert)
-    })
-    it(' Returns expected tree list when input is good', function () {
-      var ret = [{path: 'a.a', parent: 'a'}, {path: 'a.a.a', parent: 'a.a'}]
-      var assert = [{account: {parent: 'a', path: 'a.a'}, children: [{account: {path: 'a.a.a', parent: 'a.a'}, children: []}]}]
-      alphabet.buildTreeList('a', ret).should.eql(assert)
+      Alphabet.increments('').should.equal('')
     })
   })
 })
